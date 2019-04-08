33e9b5195cda4b6fc6b976955b7b954582dd59cf
Dibakar_Maji <dibakarmaji101995@gmail.com>
2019-04-08 13:17:51 +0530

    initial commit

---
+++ src/com/nt/collection/CustomCollection.java
@@ -0,0 +1,138 @@
+package com.nt.collection;
+
+import java.util.Arrays;
+
+public class CustomCollection {
+	private Object[] objArray = new Object[10];
+	private int elementCount = 0;
+
+	/**
+	 * 
+	 * @param obj
+	 */
+	public void add(Object obj) {
+		if (elementCount == objArray.length) {
+			increaseLength();
+		}
+		objArray[elementCount] = obj;
+		elementCount++;
+	}
+
+	/**
+	 * 
+	 */
+	public void increaseLength() {
+		int nextLength = 0;
+		Object[] newObjArray = null;
+		// set the nextLength value
+		nextLength = objArray.length * 2;
+		// create new Object[] object with this nextLenth
+		newObjArray = new Object[nextLength];
+		// copy each element from old object[] object
+		for (int i = 0; i < objArray.length; ++i) {
+			newObjArray[i] = objArray[i];
+		}
+		// then newObjArray reference is store into old Object[] reference variale which
+		// is now point to new object[] object
+		objArray = newObjArray;
+	}// method
+
+	/**
+	 * This Method is used to get size of the collection
+	 * @return int
+	 */
+	public int size() {
+		return elementCount;
+	}
+
+	/**
+	 * This method is used to get capacity of the collection
+	 * @return int
+	 */
+	public int capacity() {
+		return objArray.length;
+	}
+	/**
+	 * This method is use to retrive an element vale based on given index number
+	 * @param indexNum
+	 * @return
+	 */
+	public Object get(int indexNum) {
+	/*	Object obj=null;
+		if(indexNum < 0 || indexNum >= size()) {
+			throw new IndexOutOfBoundsException("Invalid Index number");
+		}
+		for(int i=0;i<objArray.length;++i) {
+			if(objArray[i]==objArray[indexNum]) {
+				obj=objArray[indexNum];
+			}
+		}
+		return obj;
+		*/
+		if(indexNum < 0 || indexNum >= size()) {
+			throw new IndexOutOfBoundsException("Invalid Index number");
+		}
+		return objArray[indexNum];
+	}
+	public String getAll() {
+		Object[] obj=new Object[size()];
+		for(int i=0;i<size();++i) {
+			obj[i]=objArray[i];
+		}
+		return Arrays.toString(obj);
+	}
+	/**
+	 * This method is used to replace the element based on given index number
+	 * @param indexNum
+	 * @param obj
+	 */
+	public void replace(int indexNum,Object obj) {
+		if(indexNum < 0 || indexNum >= size()) {
+			throw new IndexOutOfBoundsException("Invalid Index Number");
+		}
+		objArray[indexNum]=obj;
+	}
+	/**
+	 * This Method is used to remove the element from a collection
+	 * @param indexNum
+	 */
+	public void remove(int indexNum) {
+		if(indexNum < 0 || indexNum >=size()) {
+    		throw new IndexOutOfBoundsException("Invalid index number");
+    	}
+		while(indexNum<=size()-1) {
+			if(indexNum==size()-1){
+				objArray[indexNum]=null;
+			}else {
+			objArray[indexNum]=objArray[indexNum+1];
+			}
+			//increase index number
+			indexNum++;
+		}
+		//decrease array size
+		elementCount--;
+	}
+	/**
+	 * This method is used to insert an object to a colection object
+	 * @param indexNum
+	 * @param obj
+	 */
+    public void insert(int indexNum,Object obj) {
+    	if(indexNum < 0 || indexNum >=size()) {
+    		throw new IndexOutOfBoundsException("Invalid index number");
+    	}
+    	if(size()==capacity()) {
+    		increaseLength();
+    	}
+    	for(int i=size()-1;i>=indexNum;--i) {
+    		objArray[i+1]=objArray[i];
+    	}
+    	objArray[indexNum]=obj;
+    	elementCount++;
+    }
+	@Override
+	public String toString() {
+		return "CustomCollection [objArray=" + Arrays.toString(objArray) + "]";
+	}
+	
+}// class
---
+++ src/com/nt/test/CustomCollectionTest.java
@@ -0,0 +1,58 @@
+package com.nt.test;
+
+import java.awt.image.ReplicateScaleFilter;
+
+import com.nt.collection.CustomCollection;
+
+public class CustomCollectionTest {
+
+	public static void main(String[] args) {
+		CustomCollection custCollection=null;
+		//create CustomCollection class object
+		custCollection=new CustomCollection();
+		//add new object to CustomCollection object
+		custCollection.add(1);
+		custCollection.add(2);
+		custCollection.add(3);
+		custCollection.add(4);
+		custCollection.add(5);
+		custCollection.add(6);
+		custCollection.add(7);
+		custCollection.add(8);
+		custCollection.add(9);
+		custCollection.add(10);
+//		custCollection.add(11);
+		//Display CustomCollecton object size and length
+		System.out.println(custCollection);
+		
+		//invoke size() method
+		System.out.println("Size of the CustomCollection::"+custCollection.size());
+		
+		//invoke capacity() method
+		System.out.println("Capacity of the CustomCollection::"+custCollection.capacity());
+		
+		//invoke get() method
+		System.out.println("Retrive element value of given index::"+custCollection.get(1));
+		System.out.println("============================================================");
+		
+		//replace the element by given index number
+		System.out.println("Before replace::"+custCollection.getAll());
+		//invoke replace() method
+		custCollection.replace(1,"B");
+		System.out.println("Ater replace"+custCollection.getAll());
+		System.out.println("=============================================================");
+		
+/*		//remove element by index number
+		System.out.println("Before remove::"+custCollection.getAll());
+		//invoke remove method
+		custCollection.remove(1);
+		System.out.println("After remove::"+custCollection.getAll());
+	*/	
+		//insert a new object to a collection object
+		System.out.println("Before Insert::"+custCollection.getAll());
+		//invoke insert() method
+		custCollection.insert(5,"B");
+		System.out.println("After Insert::"+custCollection.getAll());
+	}
+
+}
